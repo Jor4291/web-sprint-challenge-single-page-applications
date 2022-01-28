dfd2f410142a112cd6fd9b45a5d1b6acb4419868
James Robertson <Jor4291@gmail.com>
2022-01-28 13:39:54 -0600

    setting up

--- src/App.js
+++ src/App.js
@@ -3,8 +3,19 @@ import React from "react";
 const App = () => {
   return (
     <>
-      <h1>Bloom Eats</h1>
-      <p>The best pizza on the web!</p>
+      <header>
+      <h1>Bloom Bites</h1>
+      <button href="/">Home</button>
+      </header>
+     <p>The best pizza on the web!</p>
+     
+     
+      <main>
+        <h3>Hungry? Try a Bloom Bites Pizza!</h3>
+        <button href="/pizza-form">Order Now!</button>
+      </main>
+      
+      
     </>
   );
 };
