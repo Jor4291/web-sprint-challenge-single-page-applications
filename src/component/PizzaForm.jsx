b380cf66e6fe24130a4085897e84755e8f76f1d0
James Robertson <Jor4291@gmail.com>
2022-01-28 19:25:32 -0600

    more features

--- src/component/PizzaForm.jsx
+++ src/component/PizzaForm.jsx
@@ -6,7 +6,7 @@ const PizzaForm = (props) =>{
   console.log(props)
 
 const specToppings = {
-    "PBJ" : "Pepperoni, Bacon, and Jalapeno",
+    "PBJ" : "House-Cut Pepperoni, Bill-E's Thick Cut Bacon Crumbles, and fresh picked garden Jalapenos",
     "Margherita" : "Black Market Mozzarella, Fresh Sliced Heirloom Tomatoes, and our own House-Grown Basil",
     "White" : "Creamy Sicilian Alfredo, Oven Roasted Garlic, Organic Baby Spinach, Regiano Parmigiano, Black-Market Mozzarella, and Black Truffle Oil",
     "BigPig" : "House-Made Hickory BBQ Sauce, Bill-E's Thick Cut Bacon Crumbles, Canadian Ham, Pepperoni, and Salami",
@@ -23,9 +23,22 @@ const specToppings = {
        
        <form>
         <label>
-        Name for the Order:
+        <h4>Name for the Order:</h4>
         <input type = "text"/>
         </label>
+        <br/>
+        <h4>Choose your Size</h4>
+        <label>
+            Size:
+            <select name="Size">
+                <option value ="Personal">Personal</option>
+                <option value ="Small">Small</option>
+                <option value ="Medium">Medium</option>
+                <option value ="Large">Large</option>
+                </select>
+        </label>
+        <br/>
+        <h4>Choose your Sauce</h4>
         <label>
             Sauce:
             <select name="Sauce">
@@ -37,9 +50,9 @@ const specToppings = {
         </label>
         <h4>Extra Toppings</h4>
         <label>
-           Pepperoni:
+        House-Cut Pepperoni:
             <input type = "checkbox" 
-            name = "Pepperoni"/>
+            name = "House-Cut Pepperoni"/>
         </label>
         <br/>
         <label>
@@ -59,12 +72,40 @@ const specToppings = {
             <input type = "checkbox" 
             name = "Organic Baby Spinach"/>
         </label>
+        <br/>
+        <label>
+        Black Market Mozzarella:
+            <input type = "checkbox" 
+            name = "Black Market Mozzarella"/>
+        </label>
+        <br/>
+        <label>
+           Black Truffle Oil:
+            <input type = "checkbox" 
+            name = "Black Truffle Oil"/>
+        </label>
+        <br/>
+        <label>
+          Canadian Ham:
+            <input type = "checkbox" 
+            name = " Oven Roasted Garlic"/>
+        </label>
+        <br/>
+        <label>
+           Fresh Sliced Heirloom Tomatoes:
+            <input type = "checkbox" 
+            name = "Fresh Sliced Heirloom Tomatoes"/>
+        </label>
+        <br/>
+        <label>
+        <h4>Special Instructions for the Chef!</h4>
+        <input type = "text"/>
+        </label>
         
        </form>
-    
-
-
+       <br/>
        </section>
+       
     )
 }
 
